6f54621e0f8203b690ecca99bd639482b1f56fc4
luohx <Lhongxiao@lcchain.com>
2023-08-09 15:14:07 +0800

    新增函数式编程

---
+++ javax-func/src/main/java/com/study/javax/JavaxFuncApplication.java
@@ -0,0 +1,16 @@
+package com.study.javax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luohx
+ */
+@SpringBootApplication
+public class JavaxFuncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaxFuncApplication.class, args);
+    }
+
+}
---
+++ javax-func/src/test/java/com/study/javax/JavaxFuncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.javax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaxFuncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
