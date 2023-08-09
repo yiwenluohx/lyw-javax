71022ef7b7542e456a35be2ec240f1035efe40aa
luohx <Lhongxiao@lcchain.com>
2023-08-09 17:47:27 +0800

    函数式编程小试牛刀

--- javax-func/src/main/java/com/study/javax/JavaxFuncApplication.java
+++ javax-func/src/main/java/com/study/javax/JavaxFuncApplication.java
@@ -1,5 +1,6 @@
 package com.study.javax;
 
+import com.study.javax.test01.Calculate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaxFuncApplication {
 
     public static void main(String[] args) {
+        record(() -> System.out.println("函数式编程")
+        , "数据异常,请排查");
+
         SpringApplication.run(JavaxFuncApplication.class, args);
+    }
+
+    /**
+     * 记录
+     *
+     * @param calculate 计算
+     * @param errorInfo 错误信息
+     */
+    private static void record(Calculate calculate, String errorInfo) {
+        try {
+            calculate.process();
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
 }
---
+++ javax-func/src/main/java/com/study/javax/test01/Calculate.java
@@ -0,0 +1,17 @@
+package com.study.javax.test01;
+
+/**
+ * 定义函数式接口
+ *
+ * @author luohx
+ * @version 1.0.0
+ * @date: 2023/8/9 下午4:13
+ * @menu 定义函数式接口
+ */
+@FunctionalInterface
+public interface Calculate {
+    /**
+     * 执行过程
+     */
+    void process();
+}
